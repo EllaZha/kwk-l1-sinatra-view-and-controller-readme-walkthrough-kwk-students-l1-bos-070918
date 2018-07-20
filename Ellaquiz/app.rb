b6e2850d998d834504e74c040a41aaf4e879e419
Ella Zhang <eyanchizhang@gmail.com>
2018-07-20 16:29:37 +0000

    first commit

---
+++ Ellaquiz/app.rb
@@ -0,0 +1,37 @@
+require_relative 'config/environment'
+
+class App < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'views'
+  end
+
+  get '/' do
+    erb :index
+  end 
+  
+  get '/puns' do 
+    erb :twitterpuns
+  end 
+  
+  post  '/results' do
+   answers = params.values
+   @total = 0
+   answers.each do |answer|
+     @total += answer.to_i
+   end
+   puts @total
+   
+   if @total < 6
+     erb :twitter
+   elsif @total < 9 
+     erb :instagram
+   elsif @total <11
+     erb :youtube
+   elsif @total <13 
+     erb :internet 
+   end
+ end
+    
+end 
